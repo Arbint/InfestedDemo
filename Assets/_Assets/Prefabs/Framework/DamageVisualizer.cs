3751c402524ae962f1ce7914f8ffc5bf4d1cad6e
Li <jili1@uiwtx.edu>
2025-10-07 07:36:13 -0500

    adding visuals feedback for damage

---
+++ Assets/_Assets/Prefabs/Framework/DamageVisualizer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DamageVisualizer : MonoBehaviour
+{
+    HealthComponent mHealthComponent;
+    Material mDynamicMaterial;
+    [SerializeField] Renderer mRenderer;
+    [SerializeField] Color mFlashColor;
+    [SerializeField] string mFlashColorMaterialParamName = "_EmissionAddition"; 
+
+    Color mDefaultEmissionColor;
+
+    void Awake()
+    {
+        mHealthComponent = GetComponent<HealthComponent>();
+        mHealthComponent.onTakenDamage += StartFlash;
+
+        mDynamicMaterial = new Material(mRenderer.material);
+        mRenderer.material = mDynamicMaterial;
+
+        mDefaultEmissionColor = mDynamicMaterial.GetColor(mFlashColorMaterialParamName);
+    }
+
+    private void StartFlash(float amt, GameObject instigator)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
